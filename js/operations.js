b8425d28667ac7edc8dae43a9e7439a66e207ac1
FrankPoLee <frankpolee95@gmail.com>
2016-09-19 23:30:08 -0400

    adjusted footer and copyright formatting

--- js/operations.js
+++ js/operations.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
 $('#loginbutton').click(function()
 {
+	//placeholder: check if username and password are what are desired. will change once database is implemeented
 	if((document.getElementById("usernamefield").value!="Username")||(document.getElementById("passwordfield").value!="Password")) {
 		var notifications = document.getElementById("loginnotifications");
 		notifications.innerHTML="Incorrect login or password entered";
-		//notifications.style.color="red";
 		notifications.style.border="solid rgba(255,0,0,0.2)";
 		notifications.style.backgroundColor="rgba(255,0,0,0.3)";
 	}
