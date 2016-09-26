dd0e5f055d1bd1ed2752a1f0f435480368a6c3bb
FrankPoLee <frankpolee95@gmail.com>
2016-09-26 01:08:22 -0400

    fixed email color

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // do dynamic validation for the gmu email address
-$('#passwd').bind('input', function() {
+$('#email').bind('input', function() {
 		// Our pattern is mason id
 		var regex = /.*.gmu.edu$/igm;
 		//checks users results and seets a boolean 
@@ -9,20 +9,20 @@ $('#passwd').bind('input', function() {
 			//if there is no user input do nothing
 				if ($(this).val().length == 0) {
 					//do not display any color
-						$('#passwd').removeClass('green red');
+						$('#email').removeClass('green red');
 						//no side messages
 						$(this).next().stop(true, true).fadeIn(0).html('');
 						//else
 				} else {
 					//display red and remove greeen if existing
-						$('#passwd').addClass('red').removeClass('green');
+						$('#email').addClass('red').removeClass('green');
 						//run error check, state not gmu email show red
 						$(this).next().stop(true, true).fadeIn(0).html('Not a GMU Email');
 				}
 				//else if
 		} else {
 			//display green , remove red if existing
-				$('#passwd').addClass('green').removeClass('red');
+				$('#email').addClass('green').removeClass('red');
 				//return valid email address show green
 				$(this).next().stop(true, true).fadeIn(0).html('Is a GMU Email');
 		}});//exit
