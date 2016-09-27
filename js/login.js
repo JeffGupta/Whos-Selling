d0ec9116344f25dd42d705a305e586a243864369
FrankPoLee <frankpolee95@gmail.com>
2016-09-26 21:33:17 -0400

    fixed cookie

--- js/login.js
+++ js/login.js
@@ -15,7 +15,7 @@ $(document).ready(function()
 	var today;
 	var expiry;
 	
-	$('#registersubmit').click(function()
+	$('#loginsubmit').click(function()
 	{
 		today = new Date();
 		expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
