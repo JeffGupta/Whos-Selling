1616dfccf77ed874f83a8c175ee18c73fab62038
FrankPoLee <frankpolee95@gmail.com>
2016-09-26 22:43:55 -0400

    fix

--- js/account.js
+++ js/account.js
@@ -17,8 +17,8 @@ $(document).ready(function()
 	
 	function putaccount()
 	{
-		console.log(document.cookie);
 		var username = getCookie("username");
+		console.log(username);
 		accountRef.child(username).once('value')
 			.then(function(snapshot){
 				return snapshot.val();
