d828b6892df112036d00b5a1f889769ba0c24ab0
FrankPoLee <frankpolee95@gmail.com>
2016-10-25 14:10:11 -0400

    changed index.js

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ var path = require('path');
 
 app.use(express.static('public'));
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
 
 app.listen(3000);
